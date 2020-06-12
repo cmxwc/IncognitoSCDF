caf1008270b6ad835ca01d15af58aae338fa1e1b
qy2000 <qing.yi2000@gmail.com>
2020-06-13 01:53:13 +0800

    updated table

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,24 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable(
+    {
+      searching: false,
+      ordering: false,
+      lengthChange: false,
+      info: false,
+      pageLength: 6,
+      "createdRow": function( row, data, dataIndex){
+        if( data[3] ==  "High"){
+            $(row).addClass('red');
+        }
+        if( data[3] ==  "Mid"){
+          $(row).addClass('orange');
+        }
+        if( data[3] ==  "Low"){
+        $(row).addClass('green');
+        }
+        }
+      // lengthMenu: [[5,10,-1], [5, 10, "All"]]
+    }
+  );
 });
